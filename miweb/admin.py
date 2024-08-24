6f869ee5e03a7d59d0c513c3a78ef6558d6a78b3
Nicole281 <nicole.nasg@gmail.com>
2024-08-24 19:48:21 -0400

    HITO 3 formulario contacto

--- miweb/admin.py
+++ miweb/admin.py
@@ -3,4 +3,11 @@ from .models import Flan, ContactForm
 # Register your models here.
 
 admin.site.register(Flan)
-admin.site.register(ContactForm)
+
+class ContactFormAdmin(admin.ModelAdmin):
+    readonly_fields = ('contact_form_uuid', 'customer_name', 'customer_email', 'message')
+    list_display = ('customer_name', 'customer_email', 'message', 'contact_form_uuid')
+    search_fields = ('customer_name', 'customer_email', 'message')
+
+# Register the ContactForm model with its admin class
+admin.site.register(ContactForm, ContactFormAdmin)
